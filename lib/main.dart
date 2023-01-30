60bab6fd011c26c081248814ff5527e15f0e98b7
Adarsh Ghimire <adarshghimire10@gmail.com>
2023-01-30 21:33:21 +0545

    Day 2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cataloge/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Codepur";
+
     return MaterialApp(
-        home: Material(
-            child: Center(
-      child: Text("Welcome to 30 days of learning flutter"),
-    )));
+      home: HomePage(),
+    );
   }
 }
