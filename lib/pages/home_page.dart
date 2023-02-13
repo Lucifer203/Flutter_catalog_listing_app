032efc92ab74aadd661d97bb6183022d3a98402f
Adarsh Ghimire <adarshghimire10@gmail.com>
2023-02-13 22:40:13 +0545

    UI designing

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter_cataloge/models/catalog.dart';
 import 'package:flutter_cataloge/widgets/item_widget.dart';
+import 'package:flutter_cataloge/widgets/themes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../widgets/drawer.dart';
 
@@ -38,53 +40,155 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: (CatalogeModel.items != null && CatalogeModel.items.isNotEmpty)
-            ? GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 15,
-                    crossAxisSpacing: 15),
-                itemBuilder: (context, index) {
-                  final item = CatalogeModel.items[index];
-                  return Card(
-                    clipBehavior: Clip.antiAlias,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    child: GridTile(
-                      header: Container(
-                        child: Text(
-                          item.name,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(color: Colors.deepPurple),
-                      ),
-                      child: Image.network(item.image),
-                      footer: Container(
-                        child: Text(
-                          item.price.toString(),
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(color: Colors.black),
-                      ),
-                    ),
-                  );
-                },
-                itemCount: CatalogeModel.items.length,
-              )
-            : Center(
-                child: CircularProgressIndicator(),
-              ),
-      ),
-      drawer: MyDrawer(),
+        backgroundColor: MyTheme.creamColor,
+        body: SafeArea(
+            child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CatalogHeader(),
+              if (CatalogeModel.items != null && CatalogeModel.items.isNotEmpty)
+                Expanded(child: CatalogList())
+              else
+                Center(
+                  child: CircularProgressIndicator(),
+                )
+            ],
+          ),
+        )));
+  }
+}
+
+class CatalogHeader extends StatelessWidget {
+  const CatalogHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      //mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Catalog App",
+          style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: MyTheme.darkBlueColor),
+        ),
+        Text(
+          "Trending products",
+          style: TextStyle(fontSize: 12),
+        )
+      ],
     );
   }
 }
 
-//bringvegetables({@required bool thaila,int rupee = 100});
+class CatalogList extends StatelessWidget {
+  const CatalogList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: CatalogeModel.items.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogeModel.items[index];
+        return CatalogItem(catalog: catalog);
+      },
+    );
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item catalog;
+
+  const CatalogItem({super.key, required this.catalog})
+      : assert(catalog != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(14.0),
+      // child: Card(
+      //shape: RoundedRectangleBorder(
+      //borderRadius: BorderRadius.circular(15.0),
+      //),
+      child: Container(
+        height: 100.0,
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(10)),
+        child: Row(
+          children: [
+            CatalogImage(image: catalog.image),
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  catalog.name,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: GoogleFonts.poppins().fontFamily,
+                      fontSize: 12,
+                      color: MyTheme.darkBlueColor),
+                ),
+                Text(
+                  catalog.desc,
+                  style: TextStyle(fontWeight: FontWeight.w200, fontSize: 10),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.spaceBetween,
+                  buttonPadding: EdgeInsets.fromLTRB(0, 0, 16, 0),
+                  children: [
+                    Text(
+                      "\$${catalog.price}",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: MyTheme.darkBlueColor),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Icon(Icons.shop),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(MyTheme.darkBlueColor),
+                          shape: MaterialStateProperty.all(StadiumBorder())),
+                    )
+                  ],
+                )
+              ],
+            ))
+          ],
+        ),
+      ),
+      // ),
+    );
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+
+  const CatalogImage({super.key, required this.image}) : assert(image != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        child: Image.network(image),
+        decoration: BoxDecoration(
+            color: MyTheme.creamColor, borderRadius: BorderRadius.circular(12)),
+        // color: MyTheme.creamColor,
+        padding: EdgeInsets.all(12),
+        width: MediaQuery.of(context).size.width / 3.8,
+      ),
+    );
+  }
+}
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      // fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
           color: Colors.white,
           elevation: 0.0,
@@ -17,4 +17,8 @@ class MyTheme {
   static ThemeData darkTheme(BuildContext context) => ThemeData(
         brightness: Brightness.dark,
       );
+
+  //colors
+  static Color creamColor = Color.fromARGB(255, 236, 234, 234);
+  static Color darkBlueColor = Color.fromARGB(255, 20, 3, 103);
 }
